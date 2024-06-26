eb82d5b755c3dd11496fc5f7405ef4bf62889b7c
kmj <flashbaby2@naver.com>
2024-06-26 17:38:19 +0900

    됫나?

---
+++ Gugudan_Github/src/gugudan/edu/GugudanTest.java
@@ -0,0 +1,17 @@
+package gugudan.edu;
+
+public class GugudanTest {
+	public static void main(String[] args) {
+			
+		
+			System.out.println("2단만 출력합니다");
+			System.out.println();
+		for (int i = 1; i < 10; i++) {
+			System.out.println("2"+"*"+i+"="+(2*i));
+		}
+		
+		
+		
+		
+	}
+}
